bbb771245222d9dce9fd68f609dbff03f001e182
BotzRam <108637892+BotzRam@users.noreply.github.com>
2022-08-14 13:51:18 +0700

    Update and rename message/help.js to pesan/help.js

--- pesan/help.js
+++ pesan/help.js
@@ -1,18 +1,18 @@
 exports.txtSewabot = (prefix) => {
 return `
  1-Minggu : Rp5.000
- 2-Minggu : Rp.10.000
- 1-Bulan : Rp.20.000
- 2-Bulan : Rp.30.000
- 3-Bulan : Rp40.000
+ 2-Minggu : Rp.-
+ 1-Bulan : Rp.7.000
+ 2-Bulan : Rp -
+ Permanen : Rp17.000
 `
 }
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
- _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
+ _- DANA : 085806240904_
+ _- OVO : --_
+ _- GOPAY : --_
  _- QRIS : https://bit.ly/3yy0qoZ_
 `
 }
